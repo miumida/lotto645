63bb4628514c47a97e1d6ede574da256e654f688
miumida <46196137+miumida@users.noreply.github.com>
2020-12-14 23:50:04 +0900

    Update __init__.py

--- custom_components/lotto645/__init__.py
+++ custom_components/lotto645/__init__.py
@@ -11,7 +11,6 @@ from .const import DOMAIN, PLATFORM
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.All(
-            cv.deprecated(CONF_NAME, invalidation_version="0.110"),
             vol.Schema({vol.Optional(CONF_NAME, default=DOMAIN): cv.string}),
         )
     },
